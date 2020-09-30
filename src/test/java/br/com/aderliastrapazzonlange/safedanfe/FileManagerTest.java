af962ae463d4e747f65f444ad328faf56eae807c
Aderlian Strapazzon Lange <aderlianslange@gmail.com>
2020-09-29 22:42:00 -0300

    add FileManagerTest

--- src/test/java/br/com/aderliastrapazzonlange/safedanfe/FileManagerTest.java
+++ src/test/java/br/com/aderliastrapazzonlange/safedanfe/FileManagerTest.java
@@ -3,7 +3,6 @@ package br.com.aderliastrapazzonlange.safedanfe;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.junit.After;
 import org.junit.Before;
